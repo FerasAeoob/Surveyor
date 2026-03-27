aa5503a7109692a8db239539772b4e116cdad5a4
Feras <ferasaeoob@gmail.com>
2026-03-27 19:04:27 +0300

    changes

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { inspectAttr } from 'kimi-plugin-inspect-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/Surveyor/',  // <- CHANGE THIS
   plugins: [inspectAttr(), react()],
   resolve: {
     alias: {
